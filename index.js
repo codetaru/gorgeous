7e7ae590b686d75f7a974e8c7574483b47b31187
codetaru <derrickaballen@gmail.com>
2024-02-08 20:29:17 -0600

    tested some changes

--- index.js
+++ index.js
@@ -50,15 +50,12 @@ axios.get(apiUrl).then(refreshWeather);
 }
 
 
-
-
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   
 searchCity(searchInput.value);
 }
-
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
